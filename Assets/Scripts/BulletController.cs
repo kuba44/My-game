49b0b72ec911e968b37bdb19cdc0eebe5f701c43
kuba4414 <kubabulawkin@wp.pl>
2022-05-21 13:53:17 +0200

    added bullets

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+
+    [SerializeField] float bulletSpeed = 7.5f;
+    private Rigidbody2D bulletRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletRigidbody =  GetComponent<Rigidbody2D>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bulletRigidbody.velocity = transform.right * bulletSpeed;     
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
