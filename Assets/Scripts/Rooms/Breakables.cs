8c42ba0495c016515aaf2468ac956c930f2e89d0
kuba44 <kubabulawkin@wp.pl>
2022-06-06 19:24:50 +0200

    Added broken pieces after destroying the pot

--- Assets/Scripts/Rooms/Breakables.cs
+++ Assets/Scripts/Rooms/Breakables.cs
@@ -6,6 +6,8 @@ public class Breakables : MonoBehaviour
 {
     private Animator breakableAnimator;
 
+    [SerializeField] GameObject[] brokenPiece;
+
     private void Start()
     {
         breakableAnimator = GetComponent<Animator>();
@@ -28,8 +30,20 @@ public class Breakables : MonoBehaviour
         }
     }
 
+    public void SpawnBrokenPieces()
+    {
+        foreach ( GameObject piece in brokenPiece)
+        {
+            float positionRandX = transform.position.x + Random.Range( -0.5f, 0.5f );
+            float positionRandY = transform.position.y + Random.Range( -0.25f, 0.25f );
+            float rotationRand = Random.Range( -30, 30);
+
+            Instantiate ( piece, new Vector3 ( positionRandX, positionRandY, 0 ), Quaternion.Euler ( new Vector3 ( 0, 0, rotationRand ) ) );
+        }
+    }
+
     public void Destroy()
     {
-        Destroy(gameObject);
+        Destroy ( gameObject );
     }
 }
